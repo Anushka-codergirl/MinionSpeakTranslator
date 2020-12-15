674b6f720a2fdd8ab18c49ecfc565ae0f62e9d3b
Anushka Raj <anush6633@gmail.com>
2020-12-15 19:22:14 +0530

    Adding button
    ex03: writing button click

--- js/app.js
+++ js/app.js
@@ -3,3 +3,10 @@ var username = prompt("What's your name?");
 var greetingMsg = "Welcome! " + username;
 
 alert(greetingMsg);
+
+var submitButton = document.querySelector("#submit");
+
+submitButton.addEventListener("click",() =>{
+    console.log("clicked");
+})
+
