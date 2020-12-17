3d6b287d6b0676ad0178e08aaa7f00b6262d279e
Anushka Raj <anush6633@gmail.com>
2020-12-17 21:16:07 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
+/*
+
 var username = prompt("What's your name?");
-
 var greetingMsg = "Welcome! " + username;
-
 alert(greetingMsg);
+
+*/
 
 var submitButton = document.querySelector("#input-btn");
 
@@ -10,12 +12,31 @@ var textInput = document.querySelector("#text-translator");
 
 var translateOutput = document.querySelector("#text-output");
 
-console.log(textInput);
+//console.log(textInput);
 
 var textOutput = document.querySelector("#text-output");
 
+var url = "https://api.funtranslations.com/translate/minion.json";
+
+function Translate(text){
+    return url + "?" + "text=" + text
+}
+
+function errorHandler(error){
+    console.log("Error!",error);
+}
+
+
 function clickHandler(){
-    textOutput.innerText = "dfdfdf" + textInput.value;
+   // textOutput.innerText = "Hey There!" + textInput.value;
+
+   var inputText = textInput.value;
+    
+   fetch(Translate(inputText)).then(response => response.json()).then(json => {
+       var translatedText = json.contents.translated;
+       textOutput.inputText = translatedText;
+   }).catch(errorHandler)
+
 }
 
 submitButton.addEventListener("click",clickHandler);
